af49e0b610940e87b6f7313c7f27ac5a6fe08461
Do Khac Tu <dokhactu1822@gmail.com>
2025-04-29 23:00:11 +0700

    up

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,8 +1,8 @@
 const Banner = () => {
   return (
     <section className="p_cate-head">
-        <img src="css/icon/oichin-store-inside.webp" alt="Sextoy Svakom được kết nối với app smartphone tại studio của Sextoyhn" width={2000} height={1125} />
-        <h1>Sextoyhn bán sextoy cao cấp - đồ chơi tình dục cho người lớn</h1>
+        <img src="css/icon/banner.png" alt="Sextoyhn chuyên cung cấp đồ chơi người lớn chính hãng - chất lượng cao" width={2000} height={1125} />
+        <h1>Sextoyhn chuyên cung cấp đồ chơi người lớn chính hãng - chất lượng cao</h1>
         <p>Sextoyhn bán sextoy với chủ đạo là sex toy cao cấp chính hãng của các thương hiệu đồ chơi tình dục nổi tiếng thế giới. Với hệ thống shop tình yêu khắp Việt Nam, bạn sẽ mua đồ chơi người lớn giá rẻ nhất.</p>
     </section>
   )
